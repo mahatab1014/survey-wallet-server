0d371709667792e34aedab4c887c9207880fd193
Sudip1014 <mahtabhossain1014@gmail.com>
2023-11-24 20:01:01 +0600

    servey collection

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-const cors = require("cors")
+const cors = require("cors");
+require("dotenv").config();
 const port = 5000;
 
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mquj3zk.mongodb.net/?retryWrites=true&w=majority`;
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const database = client.db("SurveyWalletDB");
+    const surveyCollection = database.collection("survey_collection");
+
+    // survey collection
+    app.get("/api/v1/surveys", async (req, res) => {
+      const results = await surveyCollection.find().toArray();
+      res.status(200).send(results);
+    });
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
